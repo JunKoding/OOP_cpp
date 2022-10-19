d43356947456d47df1676a7ca897945295a096bc
JunKoding <wnsrb0407@daum.net>
2022-10-19 09:35:02 +0900

    feat: overloading, smart pointer

--- cpp_review.cpp/review.cpp
+++ cpp_review.cpp/review.cpp
@@ -1,20 +1,41 @@
 #include <iostream>
 
-class MyClass {
-  private:
-    int a;
-    friend void f(MyClass& ref);
+
+using namespace std;
+
+
+class Matrix {
+  public:
+    Matrix() {}
+
+    Matrix(const Matrix& a) {
+      cout << "copy constructor" << endl;
+    }
+
+    Matrix(Matrix&& a) {
+      cout << "move constructor" << endl;
+    }
+
+    Matrix& operator=(const Matrix& b) {
+      cout << "copy assignment operator" << endl;
+      return* this;
+    }
+
+    Matrix& operator=(Matrix&& b) {
+      cout << "move assingment operator" << endl;
+      return* this;
+    }
 };
 
 
-void f(MyClass& ref) {
-  std::cout << ref.a << std::endl;
-}
-
-
 int main() {
-  MyClass obj;
-  f(obj);
+  Matrix x;
+  Matrix y = x;
+  Matrix z = std::move(x);
+
+  Matrix a, b ,c;
+  b = a;
+  c = std::move(a);
 
   return 0;
 }
