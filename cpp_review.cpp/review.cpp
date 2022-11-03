10edc2884b0312d20d1c3ed82c72c54b15cd1265
JunKoding <wnsrb0407@daum.net>
2022-11-03 12:15:46 +0900

    feat: extension, template 복습

--- cpp_review.cpp/review.cpp
+++ cpp_review.cpp/review.cpp
@@ -1,14 +1,94 @@
 #include <iostream>
 
 
-int main() {
-  auto l = [](int a = 30) {
-    std::cout << a << std::endl;
-  };
+//using namespace std;
 
-  l();
-  int a = 3000;
-  l(a);
+// exception 
+/*
+class MyException {};
 
-  return 0;
+
+class MyClass {
+  public:
+    ~MyClass();
+};
+
+
+MyClass::~MyClass() {
+  cout << "dtor" << endl;
 }
+
+
+void g() {
+  MyClass obj;
+  throw MyException();
+}
+
+
+void f() {
+  g();
+}
+
+
+int main() {
+  try {
+    f();
+  }
+
+  catch(const MyException& e) {
+    cout << "Exception" << endl;
+  }
+}
+*/
+
+
+// template
+/*
+template<typename T>
+void swap(T& a, T& b) {
+  T tmp = b;
+  b = a;
+  a = tmp;
+}
+
+
+int main() {
+  int a = 30;
+  int b = 50;
+  std::cout << "a: " << a << std::endl;
+  std::cout << "b: " << b << std::endl;
+
+  char c = 'c';
+  char d = 'd';
+  std::cout << "c: " << c << std::endl;
+  std::cout << "d: " << d << std::endl;
+
+  swap<int>(a,b);
+  std::cout << "a: " << a << std::endl;
+  std::cout << "b: " << b << std::endl;
+
+  swap<char>(c,d);
+  std::cout << "c: " << c << std::endl;
+  std::cout << "d: " << d << std::endl;
+}
+*/
+template<typename T>
+class NumberUtil {
+  public:
+    const T& bigger(const T& a, const T& b);
+};
+
+template<typename T>
+const T& NumberUtil<T>::bigger(const T& a, const T& b) {
+  return (a>=b) ? a:b;
+}
+
+
+int main() {
+  NumberUtil<int> intUtil;
+  std::cout << intUtil.bigger(10,20) << std::endl;
+
+  NumberUtil<double> doubleUtil;
+  std::cout << doubleUtil.bigger(10.5,20.5) << std::endl;
+}
+
