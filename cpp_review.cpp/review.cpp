f479e02a2cc128bc2e30f150b4a4315e3f049fff
JunKoding <wnsrb0407@daum.net>
2022-11-15 12:17:00 +0900

    feat: vector 복습

--- cpp_review.cpp/review.cpp
+++ cpp_review.cpp/review.cpp
@@ -1,44 +1,63 @@
-#include <string>
 #include <iostream>
+#include <vector>
 
 
 using namespace std;
 
 
+/*
 int main() {
-  string s1;
-  s1 = "test";
-
-  cout << s1.empty() << endl;
-  cout << s1.size() << endl;
-  cout << s1.length() << endl;
-  cout << s1.find('t') << endl;
+  vector<int> v1;
+  vector<int> v2(10);
+  vector<int> v3 = {1, 2, 3};
+  vector<int> v4 = v3;
   
-  for (const auto& element : s1) {
+  for(const auto& element : v4) {
     cout << "[" << element << "]" << endl;
   }
 
-  cout << s1.find('t') << endl;
+  vector<int> v5;
+  v5.push_back(1);
+  v5.push_back(2);
 
-  char buf1[1024];
-  snprintf(buf1, sizeof(buf1), "%d", 100);
-  cout << buf1 << endl;
+  for(const int& e : v5) {
+    cout << e << endl;
+  }
 
-  char buf2[1024];
-  snprintf(buf2, sizeof(buf2), "%d", 1000);
-  cout << buf2 << endl;
+  cout << v5.front() << endl;
+  cout << v5.back() << endl;
+  cout << v5.size() << endl;
+  //cout << v5.at(2) << endl; // std::out_of_range 반환
+}
+*/
 
-  string s = "100";
-  int var;
-  var = stoi(s);
-  cout << var << endl;
 
-  int i = 500;
-  string s2;
-  s2 = to_string(i);
-  cout << s2 << endl;
+int main() {
+  vector<int> v1 = {1, 2, 3};
 
-  string s3;
-  getline(cin, s3, ' ');
-  cout << s3 << endl;
+  for(vector<int>::const_iterator it = v1.cbegin(); it != v1.cend(); it++) {
+    cout << *it << endl;
+  }
+
+  vector<int> v2 = {1, 2, 3};
+
+  for(auto it = v2.begin(); it != v2.end(); it++) {
+    cout << *it << endl;
+  }
+
+  v2.insert(v2.begin(), 4);
+  v2.erase(v2.cbegin() +2);
+
+  for(vector<int>::const_iterator it = v2.cbegin(); it != v2.cend(); it++) {
+    cout << *it << endl;
+  }
+
+  vector<int> v3;
+  v3.push_back(1);
+  v3.push_back(2);
+  v3.push_back(3);
+  v3.push_back(4);
+  v3.push_back(5);
+
+  cout << v3.size() << ' ' << v3.capacity() << endl;
 }
