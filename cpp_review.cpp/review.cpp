4cb45472600373d8d4a3a0561249d146124c11f5
JunKoding <wnsrb0407@daum.net>
2022-11-22 12:40:00 +0900

    feat: shared_ptr 복습

--- cpp_review.cpp/review.cpp
+++ cpp_review.cpp/review.cpp
@@ -1,90 +1,27 @@
-#include <deque>
-#include <map>
 #include <iostream>
-#include <utility>
+#include <memory>
+#include <string>
 
 
 using namespace std;
 
 
+struct MyClass {
+  shared_ptr<MyClass> target;
+};
+
+
 int main() {
-  /*
-  deque<int> d1;
-  deque<int> d2(10);
-  deque<int> d3 = {1, 2, 3};
-  deque<int> d4 = d3;
-
-  for(const auto& e : d4) {
-    cout << e << endl;
-  }
-
-  for(deque<int>::const_iterator it = d4.cbegin(); it != d4.cend(); it++) {
-    cout << *it << endl;
-  }
-  */
-
- /*
- typedef pair<string, int> CityDistance;
- CityDistance p = {"Pusan", 325};
-
- cout << p.first << endl;
- cout << p.second << endl;
- */
-
-  // map<string, int> m;
-  // auto p = m.insert(make_pair("jun", 26));
-
-  // if(p.second) {
-  //   cout << "successful\n";
-  // } else {
-  //   cout << "failed\n";
-  // }
-
-  // p = m.emplace("seony", 21);
-
-  // if(p.second) {
-  //   cout << "successful\n";
-  // } else {
-  //   cout << "failed\n";
-  // }
-
-  // map<string, int> m = {
-  //   {"Busan", 325},
-  //   {"Incheon", 55}
-  // };
-
-  // map<string, int>::const_iterator it = m.find("Busan");
-  
-  // map<string, int> m = {
-  //   {"Busan", 325},
-  //   {"Incheon", 55}
-  // };
-  
-  // map<string, int>::const_iterator it = m.find("Busan");
-
-  // if(it != m.end()) {
-  //   cout << it->first << endl;
-  //   cout << it->second << endl;
-  // }
-
-  // it = m.find("Jeju");
-  // if(it == m.end()) {
-  //   cout << "not found\n";
-  // }
-
-  map<string,int> m = {
-    {"a", 1},
-    {"b", 2},
-    {"c", 3}
+  auto log = [](MyClass* arg) {
+    cout << "delete\n";
+    delete arg;
   };
 
-  for(map<string,int>::const_iterator it = m.cbegin(); it != m.cend(); it++) {
-    cout << it->first << endl;
-    cout << it->second << endl;
-  }
+  shared_ptr<MyClass> obj1{new MyClass, log};
+  shared_ptr<MyClass> obj2{new MyClass, log};
 
-  for(map<string,int>::value_type& p : m) {
-    cout << p.first << endl;
-    cout << p.second << endl;
-  }
+  obj1->target = obj2;
+  obj2->target = obj1;
+
+  obj1->target.reset();
 }
