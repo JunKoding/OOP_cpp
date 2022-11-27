db622f653c4ffef4edb1c10738cf3b80cf9ca7a2
JunKoding <wnsrb0407@daum.net>
2022-11-27 17:33:41 +0900

    feat: 단어개수 세기 v3 - 내림차순 정렬 추가

--- word/test3.cpp
+++ word/test3.cpp
@@ -3,33 +3,36 @@
 #include <map>
 #include <string>
 
-
 using namespace std;
 
+int main()
+{
+  int count = 1;              // 단어개수 1로 초기화
+  string str;                 // 문자열
+  map<string, int> words_map; // 단어, 단어개수 저장할 map
 
-int main() {
-  string str;
-  map<string, int> words_map;
-
-  while(cin >> str) {
-    for(int i = 0; i<str.size(); i++) {
+  while (cin >> str)
+  {
+    for (int i = 0; i < str.size(); i++)
+    {
       str[i] = tolower(str[i]);
-    } 
+    } // 모두 소문자로 변경
 
-    auto p = words_map.insert(make_pair(str, 0));
+    auto p = words_map.insert(make_pair(str, count));
 
-    if(p.second == false) {
-      
-    } else {
+    if (p.second == false)
+    {
+      ++p.first->second;
+      // 단어가 중복되면 개수 증가
+    }
+    else
+    {
       // empty
     }
   }
 
-  for(map<string,int>::const_iterator it = words_map.cbegin(); it != words_map.cend(); it++) {
+  for (map<string, int>::reverse_iterator it = words_map.rbegin(); it != words_map.rend(); it++)
+  {
     cout << it->first << " " << it->second << endl;
-  }
-
-  for(map<string,int>::const_iterator rit = words_map.rbegin(); rit != words_map.rend(); rit++) {
-    cout << rit->first << " " << rit->second << endl;
   }
 }
--- word/word.cpp
+++ word/word.cpp
@@ -7,31 +7,55 @@
 using namespace std;
 
 
-int main() {
+int main(int argc, char* argv[]) {
   int count = 1; // 단어개수 1로 초기화
   string str; // 문자열
   map<string,int> words_map; // 단어, 단어개수 저장할 map
 
-  while(cin >> str) {
-    for(int i = 0; i<str.size(); i++) {
-      str[i] = tolower(str[i]);
-    } // 모두 소문자로 변경
+  for(int i = 0; i<argc; i++) {
+    const string arg{argv[i]};
 
-    auto p = words_map.insert(make_pair(str,count));
+    if(arg == "/asc") {
+      // asc 입력하면 오름차순 정렬
+      
+      while(cin >> str) {
+        for(int i = 0; i<str.size(); i++) {
+          str[i] = tolower(str[i]);
+        } // 모두 소문자로 변경
 
-    if(p.second == false) {
-      ++p.first->second;
-      // 단어가 중복되면 개수 증가
-    } else {
-      // empty
-    } 
+        auto p = words_map.insert(make_pair(str,count));
+
+        if(p.second == false) {
+          ++p.first->second;
+          // 단어가 중복되면 개수 증가
+        } else {
+          // empty
+        } 
+      }
+
+      for(map<string,int>::const_iterator it = words_map.cbegin(); it != words_map.cend(); it++) {
+        cout << it->first << " " << it->second << endl;
+      }
+    } else if(arg == "/des"){
+        // des 입력하면 내림차순 정렬
+
+        while(cin >> str) {
+          for(int i = 0; i<str.size(); i++) {
+            str[i] = tolower(str[i]);
+          } 
+
+          auto p = words_map.insert(make_pair(str,count));
+
+          if(p.second == false) {
+            ++p.first->second;
+          } else {
+            // empty
+          } 
+        }
+
+        for(map<string,int>::const_reverse_iterator it = words_map.rbegin(); it != words_map.rend(); it++) {
+          cout << it->first << " " << it->second << endl;
+       }
+    }
   }
-
-  for(map<string,int>::const_iterator it = words_map.cbegin(); it != words_map.cend(); it++) {
-    cout << it->first << " " << it->second << endl;
-  }
-
-  // for(map<string,int>::const_iterator rit = words_map.rbegin(); rit != words_map.rend(); rit++) {
-  //   cout << rit->first << " " << rit->second << endl;
-  // }
 }
